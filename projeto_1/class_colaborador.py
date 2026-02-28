67c26bab8526c39e8e520c0f38d6c73c82533909
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-02-28 14:07:56 -0300

    Continuando o desenvolvimento do projeto

--- projeto_1/class_colaborador.py
+++ projeto_1/class_colaborador.py
@@ -1,3 +1,23 @@
-class Colaborador:
+from class_pessoa import Pessoa 
 
-    pass 
+class Colaborador(Pessoa):
+
+    """
+        Herdamos todos os métodos e atributos da classe 
+        colaborador. 
+
+    """
+
+
+    def __init__(self, nome: str= "", data_nascimento: str= "", idade: int= 0, cpf: str= "", 
+                cargo: str= "", data_admissao: str= "", ): 
+
+        # Meio que criamos um objeto Base. 
+        super().__init__(nome, data_nascimento, idade, cpf)
+
+        # E especializamos como atributos e métodos próprios. 
+        self.__cargo = 
+
+
+    int numero = 0; 
+# Escopo -> Design -> Build -> Testing -> Deploy 
